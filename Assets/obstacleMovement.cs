95e1217179af29f661cf4c666b531db94648b8f6
Jesse <jesseanderton@hotmail.com>
2020-07-15 12:36:48 +1000

    Commit
    Sorry for not doing this earlier.

---
+++ Assets/obstacleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleMovement : MonoBehaviour
+{
+    public Vector3 obsMov = new Vector3 (0, 0, -2);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localPosition (obsMov);
+    }
+}
