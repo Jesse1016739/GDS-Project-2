b008223c78ea1bf61b218055c578a79a10f066e3
Jesse <jesseanderton@hotmail.com>
2020-07-21 16:27:57 +1000

    Lots of Goodies

---
+++ Assets/Scripts/scoringSys.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class scoringSys : MonoBehaviour
+{
+    public float currentScore = 0000000000;
+    public float maxScore = 9999999999;
+
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debugging for score display
+        /*
+        if (Input.GetButtonDown("Fire1"))
+        {
+            currentScore += 1;
+        }
+        */
+
+        //Code to display the score, rounded to a whole number.
+        scoreText.text = currentScore.ToString("0");
+    }
+}
