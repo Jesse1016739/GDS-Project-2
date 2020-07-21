f51a676d664ca8c892724f3cc1651a93744eebe8
Jordan <1016971@student,sae,edu.au>
2020-07-21 15:40:09 +1000

    Script/code adding movement
    doesnt work

--- Assets/Scenes/playerMovement.cs
+++ Assets/Scenes/playerMovement.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-	Rigidbody2D rb;
+	Rigidbody rb;
 	float dirX;
 	float moveSpeed = 10f;
 
 	// Use this for initialization
 	void Start()
 	{
-		rb = GetComponent<Rigidbody2D>();
-		
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		dirX = Input.acceleration.x * moveSpeed;
-		transform.position = new Vector2(Mathf.Clamp(transform.position.x, -2, 2f), transform.position.y);
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2, 2f), transform.position.y);
 	}
 
 	void FixedUpdate()
 	{
+		rb.AddForce(0, 0, 2000 * Time.deltaTime);
 		rb.velocity = new Vector2(dirX, 0f);
 	}
 }
