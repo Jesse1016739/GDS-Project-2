26b43814618cdc60ecfaf0d261d99e01987deb2c
Jordan <1016971@student,sae,edu.au>
2020-07-22 12:16:46 +1000

    Teleport System (looping)
    Still no luck on making the character move forward along the tilting mechanic

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] Transform SpawnPoint;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = SpawnPoint.position;
+
+    }
+
+}
